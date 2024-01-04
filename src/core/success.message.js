9a5cc520028335843da9d3fecb397a5aaff90a5b
Cuong Nguyen <galvinnguyen1999@gmail.com>
2024-01-04 21:31:04 +0700

    success handler

---
+++ src/core/success.message.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const statusCode = {
+  OK: 200,
+  CREATED: 201
+}
+
+const reasonStatusCode = {
+  OK: 'Success',
+  CREATED: 'Created'
+}
+
+class SuccessResponse {
+  constructor ({
+    message,
+    status = statusCode.OK,
+    reasonStatus = reasonStatusCode.OK,
+    metadata = {}
+  }) {
+    this.message = !message ? reasonStatus : message
+    this.status = status
+    this.metadata = metadata
+  }
+
+  send (res, headers = {}) {
+    return res.status(this.status).json(this)
+  }
+}
+
+class OK extends SuccessResponse {
+  constructor ({ message, metadata }) {
+    super({ message, metadata })
+  }
+}
+
+class CREATED extends SuccessResponse {
+  constructor ({
+    message,
+    status = statusCode.CREATED,
+    reasonStatus = reasonStatusCode.CREATED,
+    metadata = {},
+    options = {}
+  }) {
+    super ({
+      message,
+      status,
+      reasonStatus,
+      metadata,
+    })
+
+    this.options = options
+  }
+}
+
+module.exports = {
+  OK,
+  CREATED
+}
